38766db7561416f5c86ccb3986b10b94544b928c
khanh1902 <khanhdt.game@gmail.com>
2022-04-26 23:05:25 +0700

    first commit

--- src/main/java/com/laptrinhjava/LichHoc/security/WebSecurityConfig.java
+++ src/main/java/com/laptrinhjava/LichHoc/security/WebSecurityConfig.java
@@ -14,7 +14,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -58,8 +57,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf()
-                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+        http.cors().and().csrf().disable();
+
         // Các trang không yêu cầu login
         http.authorizeRequests().antMatchers("api/auth/**").permitAll();
 
