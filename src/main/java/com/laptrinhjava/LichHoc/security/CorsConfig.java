bf96fcfd12e9894bb405e1af3104bde0a6ea0078
khanh1902 <khanhdt.game@gmail.com>
2022-04-26 23:14:46 +0700

    first commit

--- src/main/java/com/laptrinhjava/LichHoc/security/CorsConfig.java
+++ src/main/java/com/laptrinhjava/LichHoc/security/CorsConfig.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class CorsConfig {
 
     @Value("${allowed.origin}")
@@ -18,11 +20,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(allowedOrigin)
-                        .allowedMethods("*")
-                        .allowedHeaders("*")
-                        .allowCredentials(false)
-                        .maxAge(-1);
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH", "OPTIONS");
             }
         };
     }
