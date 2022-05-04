50c830e4a362b75b212cf8ada887fb91a3bb1571
khanh1902 <khanhdt.game@gmail.com>
2022-05-04 23:52:40 +0700

    first commit

--- src/main/java/com/laptrinhjava/LichHoc/security/CorsConfig.java
+++ src/main/java/com/laptrinhjava/LichHoc/security/CorsConfig.java
@@ -1,27 +1,27 @@
-package com.laptrinhjava.LichHoc.security;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-@EnableWebMvc
-public class CorsConfig {
-
-    @Value("${allowed.origin}")
-    private String allowedOrigin;
-
-    @Bean
-    public WebMvcConfigurer getCorsConfiguration(){
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS").allowedOrigins("*")
-                        .allowedHeaders("*");
-            }
-        };
-    }
-}
+//package com.laptrinhjava.LichHoc.security;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//@EnableWebMvc
+//public class CorsConfig {
+//
+//    @Value("${allowed.origin}")
+//    private String allowedOrigin;
+//
+//    @Bean
+//    public WebMvcConfigurer getCorsConfiguration(){
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS").allowedOrigins("*")
+//                        .allowedHeaders("*");
+//            }
+//        };
+//    }
+//}
